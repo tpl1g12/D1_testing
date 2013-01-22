248df4e15914426b6d0ac4aff1f79f8acdd25996
Tom Ladyman <tpl1g12@ecs.soton.ac.uk>
2013-01-22 17:42:42 +0000

    Working serial->parallel and valid flag
    First piece of code tested and working from today

--- system_verilog/ps2.sv
+++ system_verilog/ps2.sv
@@ -1,27 +1,31 @@
 /////////////////////////////////////////////////////////////////////
 // D1 program for keyboard input, encyption, data transfer
 // 
-// Last Updated: 20/01/13 - Removed the combinational latches.
-//		Removed parity. Still just a serial-parallel converter.
-//		
+// Last Updated: 22/01/13 - Removed the combinational latches.
+//        Removed parity. Still just a serial-parallel converter.
+//	  Fully tested and works in real life
+//        
 /////////////////////////////////////////////////////////////////////
 
-module ps2 (output logic serial_data[7:0], last_serial_data[7:0], valid, paritycheck,
-	    input logic Clk, nReset, ps2_nclk, ndata);
+module ps2 #(parameter N = 11)(output logic [N-1:0] serial_data,  
+        output logic led, valid,
+        input logic Clk, nReset, ps2_nclk, ndata, output logic [3:0] serial_counter );
 
-enum {s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,
-      s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30} present_state, next_state;
+         logic [N-4:0] last_serial_data;
+         //logic [6:0] serial_counter ;
+         
+//enum {s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,
+//      s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30} present_state, next_state;
 
-enum {t0,t1,t2,t3,t4,t5,t6,t7,t8,t9} ps2_present, ps2_next;
-	  
-//Main clock state machine	
+//enum {t0,t1,t2,t3,t4,t5,t6,t7,t8,t9} ps2_present, ps2_next;
+      
+//Main clock state machine    
 //this is for encryption and the secure wire communication  
 //should be done in another block
 always_ff @(posedge Clk, negedge nReset)
   begin: SEQ1
-	 //Add reset code
+     //Add reset code
 
-  
  end
   
 always_comb
@@ -31,113 +35,39 @@ always_comb
 
 //PS2 clock state machine
 //For getting raw data
-always_ff @(negedge ps2_nclk, negedge nReset)
+always_ff @(posedge ps2_nclk, negedge nReset)
   begin: SEQ2
-	if (!nReset)
-		begin
-		ps2_present = t0;
-		//paritycheck <= 0;
-		end
-	else
-	begin
-		ps2_present = ps2_next;
-	end
-
-	valid = 0;
-    case(ps2_present)
-     t0: begin
-		//start bit
-          if(ndata)
-			begin
-				ps2_next = t0;
-				
-			end
-          else
-			begin
-			valid = 0;
-			serial_data[0] <= 0;
-			serial_data[1] <= 0;
-			serial_data[2] <= 0;
-			serial_data[3] <= 0;
-			serial_data[4] <= 0;
-			serial_data[5] <= 0;
-			serial_data[6] <= 0;
-			serial_data[7] <= 0; 
-			//paritycheck <= 0;
-            ps2_next = t1;
-			end
-          end
-		  
-	   t1: begin
-		//First data bit
-			serial_data[7] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t2;
-			end
-          
-	   t2: begin
-		//Second data bit
-			serial_data[6] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t3;
-			end
-          
-	   t3: begin
-		//Third data bit
-			serial_data[5] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t4;
-			end
-       
-		t4: begin
-		//Fourth data bit
-			serial_data[4] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t5;
-			end
+    
+    if (!nReset)
+      begin
+      serial_data[N-1:1] <= '0;
+        led <= 1;
+        //serial_counter <= '0;
+        serial_counter <= '0;
+        valid <= 1;
+    end
+  else
+    begin
+        led <= 0;
+      if ((serial_counter < 4'b1010) )
+            begin
+                valid <= 1;
+                serial_data <= {serial_data[N-2:0], ndata};
+                serial_counter++;
+                //serial_counter <= (serial_counter+1);    
+                led <=0;
+            end
+        else
+            begin
+            serial_counter <= '0;
+            valid <= 0;
+            end
+        end
+    
+    //begin
         
-		
-		t5: begin
-		//Fifth data bit
-			serial_data[3] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t6;
-			end
-		
-		t6: begin
-		//Sixth data bit
-			serial_data[2] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t7;
-			end
-		
-		t7: begin
-		//Seventh data bit
-			serial_data[1] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t8;
-			end
-		
-		t8: begin
-		//Eighth data bit
-			serial_data[0] = ~ndata;
-			//paritycheck <= paritycheck ^ ndata;
-            ps2_next = t9;
-			end
-		
-		t9: begin
-		//Parity check and valid flag
-		//This state also throws away the stop bit by wasting a clock cycle
-			//if (paritycheck)
-				//begin
-				valid = 1;
-				last_serial_data[7:0] = serial_data [7:0];
-				//end
-            ps2_next = t0;
-			end
-		  
-	endcase
-  end
+    //end
+    end
 
 
 //parity comb
@@ -147,4 +77,3 @@ always_comb
   end
 
 endmodule
-
