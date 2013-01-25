a55da645104a6799a2897fdaa86cb4abab78e588
Tom Ladyman <tpl1g12@ecs.soton.ac.uk>
2013-01-25 02:45:44 +0000

    Commented SV and minor changes
    Might fix a lot of the problems.. might not. Changes to functionality
    are marked with "25" (the date)

--- system_verilog/ps2.sv
+++ system_verilog/ps2.sv
@@ -1,91 +1,120 @@
 /////////////////////////////////////////////////////////////////////
 // D1 program for keyboard input, encyption, data transfer
 // 
-// Last Updated: 24/01/13 - CPLD-AVR protocol
+// Last Updated: 25/01/13 - Final version (fingers crossed)
 //		
 //		
 /////////////////////////////////////////////////////////////////////
 
+/*
+
+Signal description:
+
+serial_data -> data from the ps2 in a shift register
+led -> connected to the LED
+valid -> low when data in the serial buffer is valid
+serial_out -> data output pin to AVR
+serial_clk_out -> clock output to AVR which only clocks during transmission
+serial_clk -> a divided clock to clock the AVR-CPLD protocol
+Clk -> onboard 50Mhz clock
+nReset -> active low reset
+ps2_nclk -> wrongly named clock from keyboard
+ndata -> wrongly named data from keyboard (active high)
+
+serial_buffer -> the buffer to send to the AVR
+serial_counter -> counts to 8 so the clock and data are sent once only
+test_scale -> a 25Mhz clock to test some of the clock scaling
+//^a real design would remove this as it causes HF noise
+clk_counter -> a counter to scale the clock for AVR-CPLD transmission
+
+lfsr -> shift register for the lfsr
+next_lfsr -> combinational result of taps to be fed into lfsr
+
+*/
 module ps2 #(parameter N = 11)(output logic [N-1:0] serial_data,  
 		output logic led, valid, serial_out, serial_clk_out, serial_clk,
-		
 	    input logic Clk, nReset, ps2_nclk, ndata);
 
-		 logic [N-4:0] last_serial_data;
-		 logic [N-4:0] serial_buffer;
-		 logic [3:0] serial_counter ;
-		 logic test_scale;
-		 logic [12:0] clk_counter;
+logic [N-4:0] serial_buffer;
+logic [3:0] serial_counter ;
+logic test_scale;
+logic [12:0] clk_counter;
 		 
-		 
-		 logic paritycheck;
-		 logic send_serial_flag;
-		 logic [3:0] send_serial_count;
-		 logic show_serial_clock;
+logic [3:0] send_serial_count;
+logic show_serial_clock;
 	
-//enum {s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,
-//      s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30} present_state, next_state;
-
-//enum {t0,t1,t2,t3,t4,t5,t6,t7,t8,t9} ps2_present, ps2_next;
-
 logic [16-1:0] lfsr;
 logic next_lfsr;
 
 //lfsr
-//not sure which clock.. 
+//Clocked with keyboard
+//This means the parity, start and stop will clock the lfsr too
+//this means the AVR has to account for this and increment the
+//decrypt lfsr extra times
 always_ff @(posedge ps2_nclk, negedge nReset)
-  begin: SEQ1
-  if (!nReset)
-   begin
-  lfsr[16-1:0] <= '0;
-    end
-  else
-    begin
-  lfsr <= {lfsr[16-2:0], next_lfsr};
-  end
- end
+begin: SEQ1
+	if (!nReset)
+		begin
+			//Seed to start the lfsr
+			//'0 will be unencrypted
+			lfsr[16-1:0] <= '1; //edited from '0 on 25
+		end
+	else
+		begin
+			//Shift new value in using concatenation
+			lfsr <= {lfsr[16-2:0], next_lfsr};
+		end
+end
   
 always_comb
-  begin: COM1
-    //taps at 10 12 13 15
-  //addressed from 0
-  next_lfsr = (((lfsr[13]^lfsr[15])^lfsr[12])^lfsr[10]);
-  end
+begin: COM1
+	//Addressed from 0
+	//so taps at 10 12 13 15
+	next_lfsr = (((lfsr[13]^lfsr[15])^lfsr[12])^lfsr[10]);
+end
 
   
- //clock division 
+//Clock division for CPLD-AVR
+//divide by 5000 counter for 100kHz clock
 always_ff @(posedge Clk, negedge nReset)
-  begin: SEQ4
-  if (!nReset)
-  begin
-   clk_counter <= '0;
-   serial_clk<= '0;
-  end
- else
-  begin
-   if ((clk_counter < 8'b11111001) )//8'b11111001 for 100khz.. 10'b1111111110 was good
-    begin
-     clk_counter++;
-    end
-   else
-    begin
-     clk_counter <= '0;
-      serial_clk<= ~serial_clk;
-    end
-  end
- end
+begin: SEQ4
+	if (!nReset)
+	begin
+		clk_counter <= '0;
+		serial_clk<= '0;
+	end
+	else
+	begin
+		//Count to 5000
+		if ((clk_counter < 8'b11111001) )
+		begin
+			clk_counter++;
+		end
+		else
+		begin
+			clk_counter <= '0;
+			//invert the output
+			serial_clk<= ~serial_clk;
+		end
+	end
+end
+
 always_comb
-  begin: COM6
-		test_scale = clk_counter[0];
-  end
+begin: COM6
+	//Purely a testing routine
+	//LSBs of counters oscillate
+	test_scale = clk_counter[0];
+end
   
-  
-  
-  //change this clock
+//Poll valid for when it goes low
+//enable the show_serial_clock flag
+//count to 8 and output each bit
+//reset the counter when data is no longer valid (~valid)
+//....Poll valid..
 always_ff @(negedge serial_clk,negedge nReset)
 begin: SEQ6
 	if (!nReset)
-   begin
+	begin
 		//send_serial_flag <= '0;
 		send_serial_count <= '0;
 		show_serial_clock <= '0;
@@ -164,6 +193,8 @@ begin: SEQ6
 			show_serial_clock <= '0;
 			send_serial_count++;
 		end
+		//Dumy state to act as a hold time
+		//Was not needed as AVR clocked on rising edge
 		/*else
 		begin
 			show_serial_clock <= '0;
@@ -172,72 +203,73 @@ begin: SEQ6
 	end
 end
 
+//AND the clock with the show flag to only show
+//the clock 8 times as controlled by the previous ff block
 always_comb
-  begin: COM7//////////////////
-    serial_clk_out = serial_clk & show_serial_clock;
-  end
-	  
-//Main clock state machine	
-//this is for encryption and the secure wire communication  
-//should be done in another block
-always_ff @(posedge Clk, negedge nReset)
-  begin: SEQ5
-	 //Add reset code
+begin: COM7
+	serial_clk_out = serial_clk & show_serial_clock;
+end
 
- end
-  
+//Removed this assignment
+//the buffer should be unchanged between
+//valid cycles so that it appears to be clocked
+//by the valid flag
+//This allows processes to poll the valid flag or
+//be clocked by it and always have valid information
+//It is instead set in the next block at the same time as the valid flag
 always_comb
-  begin: COM5
-    serial_buffer = (serial_data[9:1] ^lfsr[15:7]);/////////////////rachel's on a roll
-  end
+begin: COM5
+    //serial_buffer = (serial_data[9:1] ^lfsr[15:7]);
+end
 
-//PS2 clock state machine
+//PS2 clocked shift register
 //For getting raw data
 always_ff @(negedge ps2_nclk, negedge nReset)
-  begin: SEQ2
-	
+begin: SEQ2
 	if (!nReset)
-	  begin
-      serial_data[N-1:1] <= '0;
+	begin
+		//Just a reset but light the LED for peace of mind
+		serial_data[N-1:0] <= '0; //edited from :1 to :0 on 25
 		led <= 1;
-		//serial_counter <= '0;
 		serial_counter <= '0;
 		valid <= 1;
     end
-  else
+	else
     begin
 		led <= 0;
-      if ((serial_counter < 4'b1010) )
-			begin
-				valid <= '1; //'1
-				serial_data <= {serial_data[N-2:0], ndata};
-				serial_counter <= (serial_counter+1);	
-				led <=0;
-			end
+		//Count to 8 to toggle valid at the end of each byte
+		if ((serial_counter < 4'b1010) )
+		begin
+			valid <= '1; 
+			serial_data <= {serial_data[N-2:0], ndata};
+			serial_counter <= (serial_counter+1);	
+			led <=0;
+		end
 		else
-			begin
+		begin
 			serial_counter <= '0;
 			valid <= '0;
-			end
+			//Write the encrypted byte to the buffer
+			//ignoring start, stop, parity and the wasted
+			//combinations of the lfsr due to the clock
+			//running for these bits but these bits not being sent.
+			//This means the real XOR data is not truly an lfsr
+			//as some states are missed out.
+			//Security through obscurity? ;)
+			//.........
+			//OK, no.
+			serial_buffer = (serial_data[9:1] ^lfsr[15:7]);
 		end
-	
-	//begin
-		
-	//end
 	end
+end
 
-
-//parity comb
+//Commented out to remove complexity but should cause no problems
+//This should be NOT'ed then AND'ed with the valid flag
+//as it is 1 when parity is odd valid should be 0 when data is valid
 always_comb
-  begin: COM2
-    //paritycheck = ((((((((serial_data[9]^serial_data[8])^serial_data[7])^serial_data[6])^serial_data[5])^serial_data[4])^serial_data[3])^serial_data[2])^serial_data[1]);
-  end
-
-  
-  
-  
-  
-  
+begin: COM2
+	//paritycheck = ((((((((serial_data[9]^serial_data[8])^serial_data[7])^serial_data[6])^serial_data[5])^serial_data[4])^serial_data[3])^serial_data[2])^serial_data[1]);
+end
 endmodule
 
 
--- system_verilog/test_ps2.sv
+++ system_verilog/test_ps2.sv
@@ -1,22 +1,30 @@
 module test_ps2();
 
-logic serial_data[7:0], last_serial_data[7:0], valid, paritycheck;
+logic [11-1:0] serial_data;
+logic led, valid, serial_out, serial_clk_out, serial_clk;
 logic Clk, nReset, ps2_nclk, ndata;
 
 ps2 c (.*); // create counter instance
 
-//always // clock process
-//begin
-//The clock is only active on data send for keyboard
-//so this isn't used until the program does more than
-//just interface with the keyboard.
-//end
+always // clock process
+begin
+//main onboard fast clock
+#1ns Clk = 0;
+#1ns Clk = 1;
+end
+
+
 
 initial // test module
 begin
+	
+	#2ns nReset = 1;
+	#2ns nReset = 0;
+	#2ns nReset = 1;
+
 	#10ns  ps2_nclk = 1;
 	#10ns  ndata = 0;
-	#250ns nReset = 1;
+	//#250ns nReset = 1;
 	//Clock in 1 11111111 1 1
 	#10ns  ps2_nclk = 0;
 	#10ns  ps2_nclk = 1;
@@ -67,6 +75,28 @@ begin
 	#10ns  ps2_nclk = 0;
 	#10ns  ps2_nclk = 1;
 	#10ns  ps2_nclk = 0;
+	
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	#10ns  ps2_nclk = 1;
+	#10ns  ps2_nclk = 0;
+	
 	
 end
 endmodule
